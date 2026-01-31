8a141b39abb33c529bb21aa7669b003c96e683d5
Stephany <steph_sap@outlook.es>
2026-01-30 21:39:36 -0600

    Implement layout for shell component with sidebar and main content area

--- cafesito-pos/frontend/src/layout/shell/shell.component.ts
+++ cafesito-pos/frontend/src/layout/shell/shell.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from "../sidebar/sidebar.component";
 
 @Component({
   selector: 'app-shell',
-  imports: [],
+  imports: [RouterOutlet, SidebarComponent],
   templateUrl: './shell.component.html',
   styleUrl: './shell.component.css'
 })
